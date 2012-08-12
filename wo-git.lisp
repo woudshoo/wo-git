c5f2b0f129d23501a893e51a5923f267d2fbcb8a
Willem Rein Oudshoorn <woudshoo+github@xs4all.nl>
2012-08-12 16:44:09 +0200

    Imroved documentation.
    Signed-off-by: Willem Rein Oudshoorn <woudshoo+github@xs4all.nl>

--- wo-git.lisp
+++ wo-git.lisp
@@ -38,9 +38,6 @@ An example of use would be:
       (process-close proc))))
 
 
-#+nil (defun oid-to-string (oid)
-  (format nil "~40,'0X" oid))
-
 (defun git-names ()
   "Returns a hash table which maps commit oid to a list of names.
 The precondition is that the git repository is already opened"
@@ -64,7 +61,16 @@ The precondition is that the git repository is already opened"
 
 (defclass git-graph (simple-graph)
   ((name-map :accessor name-map)
-   (reverse-name-map :accessor reverse-name-map)))
+   (reverse-name-map :accessor reverse-name-map))
+  (:documentation
+   "Representing the git revision graphs.  Nodes are OIDs (which are integers)
+and the vertices are the default vertices (that is #(OID1 OID2)).
+
+It also has two maps, one from name to OID and one from OID to name.
+
+A name can be any name refering to an OID, but the default is to have all tags and branches
+as a name, in addition to the generated names of the form `Initial-0' etc which name
+the initial commits in the repository."))
 
 
 (defmethod copy ((graph git-graph))
@@ -77,18 +83,31 @@ The precondition is that the git repository is already opened"
 ;;; Creation of the graph
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun remove-error-generating-references (references)
-  (format t "Length of input references: ~D~%" (length references))
+  "This is an helper function which works around bugs in libgit2.
+It seems that some tags not containing any message will generate an error.
+This method takes a list of references (names like branches and tags) and
+tries to lookup the corresponding commit. All references which generate an error
+during lookup are removed from the result."
   (let ((result (loop :for reference :in references
-		   :when (handler-case  (cl-git::commit-oid-from-oid (cl-git::lookup-oid :head reference)) 
+		   :when (handler-case  (cl-git::commit-oid-from-oid (cl-git::lookup-oid :head reference))
 			   (condition (x) (format t "Got error for: ~A and the error is: ~A~%" reference x) nil))
 		   :collect reference)))
-    (format t "Length of output references: ~D~%" (length result))
     result))
-       
+
 
 ;;;;;;
 (defun get-git-graph (git-dir)
-  "Creates a graph based on the repository in the `git-dir'"
+  "Creates a graph based on the repository in the `git-dir'.
+It will create this graph by starting with all references it can find and follow
+the parents of those references recursively until it has build the whole graph.
+
+All the names found by `cl-git:git-reference-listall' are used as starting point
+and will be kept as names for later symbolic lookup with `name-to-vertex'.
+
+In addition, for each commit which does not have a parent, a fake name of the form
+`Initial-~D' is created.
+
+The return value is of the type `git-graph'."
   (let ((graph (make-instance 'git-graph)))
     (cl-git:with-repository (git-dir)
       (cl-git:with-git-revisions
@@ -143,11 +162,11 @@ or a name for the commit.  If there is a name with which is also
 a SHA1 for a different commit, the named vertex will be returned.
 
 If the value is neither a valid name, nor a valid SHA1 it still returns a value, but
-this value is not a vertex in the graph.  So error recovery from this function is 
+this value is not a vertex in the graph.  So error recovery from this function is
 not easily done."
   (typecase name-or-rev
     (string
-     (or (name-to-vertex name-or-rev git-graph) 
+     (or (name-to-vertex name-or-rev git-graph)
 	 (parse-integer name-or-rev :radix 16 :junk-allowed t)))
     (t nil)))
 
@@ -164,5 +183,3 @@ have eiter no parents or no children"
      :for vertex = (name-to-vertex name git-graph)
      :unless (and (targets-of-vertex vertex git-graph) (sources-of-vertex vertex git-graph))
      :collect name))
-
-
